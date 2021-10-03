c3057d2790c99dbde66f10b5f4801ad2aedc5d4a
RinatMagnat <50412433+RinatMagnat@users.noreply.github.com>
2021-10-03 16:40:00 +0500

    1 Сделана версия для консольного калькулятора с распариванием строки и действиями умножения, деления, сложения, вычитания.

---
+++ ConsolCalc/ConsolCalc/Calc.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolCalc
+{
+    class Calc
+    {
+        string[] simicolon;
+        float[] number;
+       
+        #region Конструктор
+        public Calc(string st)
+        { int simicolon_count = 0;
+            for (int i = 0; i < st.Length; i++) 
+            {
+                if (st[i] == '+' || st[i] == '*' || st[i] == '/' || st[i] == '-') 
+                {
+                    simicolon_count++;
+                }
+            }
+            simicolon = new string[simicolon_count];
+            number = new float[simicolon_count+1];
+            ParseString(st);
+            AnswerMultiplayAndDivided();
+            AnswerPlusAndMinus();
+            
+        }
+        #endregion
+
+        #region Парсим строку 
+        private void ParseString(string st) 
+        {
+            int index_number = 0;
+            int index_simicolon = 0;
+
+            string temp="";
+            for (int i = 0; i < st.Length; i++) 
+            {
+                if (i == st.Length - 1)
+                {
+                    temp += st[i].ToString();
+                    number[index_number] += float.Parse(temp); 
+                }
+                if (st[i] == '+' || st[i] == '*' || st[i] == '/' || st[i] == '-' )
+                {                  
+                    simicolon[index_simicolon] += st[i];
+                    number[index_number] += float.Parse(temp);
+                    temp = "";
+                    index_simicolon++;
+                    index_number++;
+                }
+                else 
+                {
+                    temp += st[i].ToString();                    
+                }
+            }
+        }
+        #endregion
+
+        #region умножение и деление
+        private void AnswerMultiplayAndDivided() 
+        {
+            for (int i = 0; i < simicolon.Length; i++) 
+            {
+                if (simicolon[i] == '/'.ToString()) 
+                {
+                    number[i+1] = number[i] / number[i + 1];
+                    number[i] = 0;
+                    simicolon[i] = '+'.ToString();
+                }
+                if (simicolon[i] == '*'.ToString()) 
+                {
+                    number[i+1] = number[i] * number[i + 1];
+                    number[i] = 0;
+                    simicolon[i] = '+'.ToString();
+                }
+            }
+        }
+        #endregion
+
+        private void AnswerPlusAndMinus() 
+        {
+            for(int i = 0; i<simicolon.Length; i++)
+            {
+                if(simicolon[i] == '+'.ToString())
+                {
+                    number[i + 1] = number[i] + number[i + 1];
+                }
+                if(simicolon[i] == '-'.ToString())
+                {
+                    number[i + 1] = number[i] - number[i + 1];
+                }
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(" Answer " + number[number.Length-1]);
+        }
+    }
+}
---
+++ ConsolCalc/ConsolCalc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Введите значение ");
+            Console.Write(" ");
+            string consol_string = Console.ReadLine();
+            Calc calc = new Calc(consol_string);
+            calc.Display();
+            Console.ReadLine();
+        }
+    }
+}
